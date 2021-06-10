f12b194e2aaa118975378407ec6455e190e0d3e8
avishssarve <85667748+avishssarve@users.noreply.github.com>
2021-06-10 18:15:03 +0530

    Add files via upload

---
+++ PetBean.java
@@ -0,0 +1,51 @@
+package com.casestudy.model;
+
+public class PetBean {
+
+	private Long id;
+	private Long userId;
+	private String name;
+	private String age;
+	private String place;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+}
---
+++ RegistrationBean.java
@@ -0,0 +1,41 @@
+package com.casestudy.model;
+
+public class RegistrationBean {
+	private Long userId;
+	private String userName;
+	private String password;
+	private String confirmPassword;
+	
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+}
